ac70717e4f0d29f6592090b664255e2cd4066733
xaviac <jacevedomx@gmail.com>
2021-10-19 20:40:18 -0500

    First commit

---
+++ Leccion01/01-HolaMundo.js
@@ -0,0 +1,3 @@
+let name = "Javier";
+
+console.log(name)
---
+++ Leccion01/02-01-TiposDatosJavaScript.js
@@ -0,0 +1,14 @@
+/* 
+Ejemplos de
+tipos de datos
+en JavaScript
+*/
+// Tipo de dato String
+let name = "Xavier"
+console.log(name)
+
+// Tipo de dato number
+let number = 1000;
+console.log(number)
+
+// Tipo de dato Objeto
