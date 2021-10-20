8bac2d14d25ba5bc2e6ff43301f393369868cc2f
xaviac <jacevedomx@gmail.com>
2021-10-20 14:55:32 -0500

    Precedencia de operadores

---
+++ Leccion03/03-03-PrecedenciaDeOperadores.js
@@ -0,0 +1,14 @@
+let a = 3, b = 2, c = 1;
+let z = a * b;
+console.log(z);
+
+z = a * b + c;
+console.log(z);
+z = a * (b + c);
+console.log(z);
+
+z =  c + a * b;
+console.log(z);
+z =  (c + a) * b;
+console.log(z);
+
