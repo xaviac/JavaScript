8bf4dd9140ecdb49957bce43dc60413307c83517
xaviac <jacevedomx@gmail.com>
2021-10-19 22:06:09 -0500

    Tipos de datos

--- Leccion01/02-01-TiposDatosJavaScript.js
+++ Leccion01/02-01-TiposDatosJavaScript.js
@@ -4,11 +4,21 @@ tipos de datos
 en JavaScript
 */
 // Tipo de dato String
-let name = "Xavier"
-console.log(name)
+let nombre = "Xavier"
+console.log(typeof nombre)
+
+nombre = 10;
+console.log(nombre)
+console.log(typeof nombre)
 
 // Tipo de dato number
 let number = 1000;
 console.log(number)
 
 // Tipo de dato Objeto
+let object = {
+    nombre: "Javier",
+    apellido: "Pérez",
+    telefono: "5566778899"
+}
+console.log(object)
